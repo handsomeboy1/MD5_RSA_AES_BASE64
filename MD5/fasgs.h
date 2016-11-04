db918a5da50a7765828b594d008a8363cb635918
姚强红 <yaoqianghong@yaoqianghongdeiMac.local>
2016-11-04 13:59:46 +0800

    second

---
+++ MD5/fasgs.h
@@ -0,0 +1,13 @@
+//
+//  fasgs.h
+//  MD5
+//
+//  Created by 姚强红 on 16/11/4.
+//  Copyright © 2016年 eHai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fasgs : UIView
+
+@end
---
+++ MD5/fasgs.m
@@ -0,0 +1,21 @@
+//
+//  fasgs.m
+//  MD5
+//
+//  Created by 姚强红 on 16/11/4.
+//  Copyright © 2016年 eHai. All rights reserved.
+//
+
+#import "fasgs.h"
+
+@implementation fasgs
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
